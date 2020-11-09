327cd92dd5f1b06d6097c0295e6e670ef670b352
Noritaka IZUMI <noritaka.izumi@gmail.com>
2020-11-09 14:05:44 +0900

    関数のファイル移動。

--- pkg/linked_list/model.go
+++ pkg/linked_list/model.go
@@ -1,5 +1,10 @@
 package linked_list
 
+import (
+	"errors"
+	"word_games/pkg/message"
+)
+
 func ConvertWordToLinkedList(word Word) *LinkedList {
 	lst := GetEmptyLinkedList()
 	for _, c := range word {
@@ -24,6 +29,36 @@ func (rcv *Word) IsPalindrome() (ret bool) {
 	lst2, _ := lst1.MidCut()
 	ret, _ = lst1.IsEqualTo(lst2)
 	return ret
+}
+
+// IsEqualTo は 2 つのリストが持つ値の順序が一致しているかどうかを判断します。
+// 一致していれば true 、一致していなければ false を返します。
+//
+// ノードの個数が異なる 2 つのリストは、前方のノードの値によらず一致していないとみなされます。
+// ノードがない 2 つのリストは一致しているとみなされます。
+//
+// 一方のノードが nil の場合、エラーを返します。
+func (rcv *LinkedList) IsEqualTo(tgt *LinkedList) (ret bool, err error) {
+	if rcv == nil || tgt == nil {
+		return false, errors.New(message.Nil)
+	}
+	count := 0
+	for rcv.Head != nil && tgt.Head != nil {
+		count++
+		if rcv.Head.Val != tgt.Head.Val {
+			return false, nil
+		}
+		_ = rcv.DeleteAtHead()
+		_ = tgt.DeleteAtHead()
+	}
+	// NOTE: for 文についての C1 カバレッジを測定するため。
+	if count == 0 {
+		// for 文の中に入らなかった
+	}
+	if rcv.Head == nil && tgt.Head == nil {
+		return true, nil
+	}
+	return false, nil
 }
 
 func GetCommonEnding(word1 *Word, word2 *Word) *Word {
