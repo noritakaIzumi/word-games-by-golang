a1f4ebe4734e21728385195d9acb529ef966ba40
Noritaka IZUMI <noritaka.izumi@gmail.com>
2020-11-09 10:52:57 +0900

    Main 関数の実装とエントリポイントの定義。

--- cmd/get_common_ending/main.go
+++ cmd/get_common_ending/main.go
@@ -1,5 +1,20 @@
 package get_common_ending
 
+import (
+	"fmt"
+	"word_games/internal"
+	"word_games/pkg/io"
+	"word_games/pkg/message"
+)
+
 func Main() {
-	// TODO: 処理を記述する
+	firstWord := io.GetInput(message.PleaseEnterFirstWord)
+	secondWord := io.GetInput(message.PleaseEnterSecondWord)
+
+	hasCe, ce := internal.GetCommonEnding(firstWord, secondWord)
+	if hasCe {
+		fmt.Printf(message.FmtWordsHaveCommonEnding, firstWord, secondWord, ce)
+	} else {
+		fmt.Printf(message.FmtWordsHaveNoCommonEndings, firstWord, secondWord)
+	}
 }
--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"word_games/cmd/get_common_ending"
 	"word_games/cmd/is_palindrome"
 )
 
@@ -12,5 +13,7 @@ func main() {
 	switch appName {
 	case "is_palindrome":
 		is_palindrome.Main()
+	case "get_common_ending":
+		get_common_ending.Main()
 	}
 }
